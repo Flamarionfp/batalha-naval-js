bbcddff311adaec3885001d6e0677e15508c0ef0
flamarionfp <flamarionpinto@sou.faccat.br>
2023-05-11 22:23:42 -0300

    feat: build ship function and fill board

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,13 @@
 import { letters } from "./constants";
 
+type Direction = "horizontal" | "vertical";
+
 const board: Array<string[]> = [[]];
-
 const BOARD_SIZE = 11;
-
-function initBoard() {
-  for (let i = 0; i < BOARD_SIZE; i++) {
-    board[i] = [];
-    for (let j = 0; j < BOARD_SIZE; j++) {
-      board[i][j] = "~";
-    }
-  }
-}
+const SHIPPING_LENGTH = 5;
+const EMPTY_IDENTIFIER = "~";
+const SHIP_IDENTIFIER = "S";
+// const FIRE_IDENTIFIER = "X"; // use later
 
 function _createBox() {
   const box = document.createElement("div");
@@ -19,11 +15,13 @@ function _createBox() {
   return box;
 }
 
-function _createTextContent(content: string) {
-  const textContentElement = document.createElement("span");
-  textContentElement.textContent = content;
-
-  return textContentElement;
+function initBoard() {
+  for (let i = 0; i < BOARD_SIZE; i++) {
+    board[i] = [];
+    for (let j = 0; j < BOARD_SIZE; j++) {
+      board[i][j] = EMPTY_IDENTIFIER;
+    }
+  }
 }
 
 function drawBoard() {
@@ -34,13 +32,13 @@ function drawBoard() {
       const box = _createBox();
 
       if (i === 0 && j > 0) {
-        const numberElement = _createTextContent(String(j));
-        box.appendChild(numberElement);
-      }
-
-      if (j === 0 && i > 0) {
-        const letterElement = _createTextContent(letters[i - 1]);
-        box.appendChild(letterElement);
+        box.textContent = String(j);
+      } else if (j === 0 && i > 0) {
+        box.textContent = letters[i - 1];
+      } else if (i !== 0 && j !== 0) {
+        box.textContent = board[i][j];
+      } else {
+        box.innerHTML = "";
       }
 
       if (boardElement) {
@@ -50,8 +48,27 @@ function drawBoard() {
   }
 }
 
+function buildShip(
+  startsAtColumn: number,
+  startsAtRow: number,
+  direction: Direction,
+  shipLength: number
+) {
+  for (let i = startsAtColumn; i < shipLength + startsAtColumn; i++) {
+    if (direction === "horizontal") {
+      board[startsAtRow][i] = SHIP_IDENTIFIER;
+    } else {
+      board[i][startsAtColumn] = SHIP_IDENTIFIER;
+    }
+  }
+}
+
 function main() {
   initBoard();
+  const startsAtColumn = 2; // user must inform
+  const startsAtRow = 4; // user must inform
+  const direction: Direction = "horizontal"; // user must inform
+  buildShip(startsAtColumn, startsAtRow, direction, SHIPPING_LENGTH);
   drawBoard();
   console.log(board);
 }
